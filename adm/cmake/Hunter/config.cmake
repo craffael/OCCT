e28f7ec2df97f859ed318b82143973711ed4f3bd
craffael <raffael@casagrande.ch>
2020-08-28 09:01:18 +0200

    hunterify occt

---
+++ adm/cmake/Hunter/config.cmake
@@ -0,0 +1,8 @@
+set(build_shared_libs OFF)
+if("${BUILD_LIBRARY_TYPE}" STREQUAL "Shared")
+  set(build_shared_libs ON)
+endif()
+hunter_config(freetype 
+VERSION "2.9.1-p1"
+CMAKE_ARGS
+BUILD_SHARED_LIBS=${build_shared_libs})
