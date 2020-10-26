93935250a01f3d50c6db254bfc87f30a32ed4db9
craffael <raffael@casagrande.ch>
2020-10-26 17:31:58 +0100

    echo value of build_shared_libs

--- adm/cmake/Hunter/config.cmake
+++ adm/cmake/Hunter/config.cmake
@@ -2,6 +2,7 @@ set(build_shared_libs OFF)
 if("${BUILD_LIBRARY_TYPE}" STREQUAL "Shared")
   set(build_shared_libs ON)
 endif()
+message("BUILD_SHARED_LIBS = ${build_shared_libs}")
 hunter_config(freetype 
 VERSION "2.9.1-p1"
 CMAKE_ARGS
